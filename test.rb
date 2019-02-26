8fa68e4fbdec52d462da49010f0f9369f1fc0dc7
승근 현 <sk@mynameismira.com>
2019-02-26 14:36:40 +0900

    my first util commit

---
+++ test.rb
@@ -0,0 +1,28 @@
+require './util.rb'
+
+def test_parsedate() 
+	dt = parsedate('Feb/26/19')
+	debug(dt)
+
+	dt = parsedate('26/Feb/19')
+	debug(dt)
+
+	dt = parsedate('Feb/26/1999')
+	debug(dt)
+
+	dt = parsedate('26/Feb/1980')
+	debug(dt)
+
+	dt = parsedate('2019-2-26')
+	debug(dt)
+
+	debug(getage('1973-12-19'))
+end
+
+def test_loadjson() 
+	dic = loadjson('E:\myCoding\python\projects\utils\python_labs\env\Lib\site-packages\pycodestyle-2.5.0.dist-info\metadata.json')
+	debug('test', 'debug', dic)	
+end
+#puts(JSON.pretty_generate(dic))
+test_parsedate
+test_loadjson
